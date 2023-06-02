f0d49f98eb0c86674a71b9487421789bf720546b
gty <gefyoung@gmail.com>
2023-06-02 13:49:15 +1000

    mobile formatted separator

--- app/components/separator/caseStudy.tsx
+++ app/components/separator/caseStudy.tsx
@@ -2,16 +2,18 @@ import Image from "next/image"
 import LockCrosshair from "/public/separator/lockCrosshair.png"
 import Button from "../lib/button"
 
-export default function WhitePaper() {
+export default function CaseStudy() {
   return (
     <div>
       <div className="flex justify-center mb-6">
-      <Image src={LockCrosshair} alt="Lock crosshair"></Image>
+        <Image src={LockCrosshair} alt="Lock crosshair"></Image>
       </div>
-      <div className="max-w-xl text-4xl leading-[3rem] text-center text-white font-extralight">
-        <div>Cybersecurity and Financial Services</div>
+      <div className="flex justify-center">
+        <div className="max-w-xl text-3xl md:text-4xl leading-[3rem] text-center text-white font-extralight">
+          <div>Cybersecurity and Financial Services</div>
+        </div>
       </div>
-      <div className="flex justify-center mt-[108px]">
+      <div className="flex justify-center mt-10 md:mt-[108px]">
         <Button color="green">DOWNLOAD WHITE PAPER</Button>
       </div>
     </div>
--- app/components/separator/whitePaper.tsx
+++ app/components/separator/whitePaper.tsx
@@ -2,18 +2,20 @@ import Image from "next/image"
 import CheckList from "/public/separator/padIcon.png"
 import Button from "../lib/button"
 
-export default function CaseStudy() {
+export default function WhitePaper() {
   return (
     <div className="">
       <div className="flex justify-center mb-6">
         <Image src={CheckList} alt="checklist"></Image>
       </div>
-
-      <div className="max-w-xl text-4xl leading-[3rem] text-center text-white font-extralight">
-        <div>What Defense Contractors need</div>
-        {" to know about the US Government's CMMC requirements"}
+      <div className="flex justify-center">
+        <div className="max-w-xl text-3xl md:text-4xl leading-[3rem] text-center text-white font-extralight">
+          <div>What Defense Contractors need</div>
+          {" to know about the US Government's CMMC requirements"}
+        </div>
+        {/* <div className="flex justify-center mt-6"></div> */}
       </div>
-      <div className="flex justify-center mt-6">
+      <div className="flex justify-center mt-10">
         <Button color="green">DOWNLOAD WHITE PAPER</Button>
       </div>
     </div>
